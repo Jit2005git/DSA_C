ab884f711a6af99ee3823458cb7883262b1980a0
Jit2005git <jitghosh304@gmail.com>
2026-06-02 21:47:12 +0530

    er

--- binarySearch/binary_usingrecursion.c
+++ binarySearch/binary_usingrecursion.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
-int binarySearch(int arr[], int l, int r, int x) {
+int recursivebinarySearch(int arr[], int l, int r, int x) {
     if (r >= l) {
         int mid = l + (r - l) / 2;
         if (arr[mid] == x)
             return mid;
         if (arr[mid] > x)
-            return binarySearch(arr, l, mid - 1, x);
-        return binarySearch(arr, mid + 1, r, x);
+            return recursivebinarySearch(arr, l, mid - 1, x);
+        return recursivebinarySearch(arr, mid + 1, r, x);
     }
     return -1;
 }
@@ -15,10 +15,10 @@ int main() {
     int arr[] = {2, 4, 6, 8, 10};
     int n = sizeof(arr) / sizeof(arr[0]);
     int x = 6;
-    int result = binarySearch(arr, 0, n - 1, x);
+    int result = recursivebinarySearch(arr, 0, n - 1, x);
     if (result == -1)
         printf("Element not found in array\n");
     else
         printf("Element found at index %d\n", result);
     return 0;
-}
+}   
