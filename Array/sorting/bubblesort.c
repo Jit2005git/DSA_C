b9a25b02e4b0a8d46b0e76d6b885e7a757642a4f
Jit2005git <jitghosh304@gmail.com>
2026-06-16 15:39:48 +0530

    bubble2

--- Array/sorting/bubblesort.c
+++ Array/sorting/bubblesort.c
@@ -12,9 +12,18 @@ void bubblesort(int arr[], int n) {
     }
 }
 int main(){
-    int arr[];
+    int arr[100];
     int n;
     printf("Enter the number of elements: ");
     scanf("%d", &n);
     printf("Enter the elements: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    bubblesort(arr, n);
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
 }
