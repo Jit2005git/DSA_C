72b3d823a370a4d79a41d12bd8c755281e9ec211
Jit2005git <jitghosh304@gmail.com>
2026-06-16 15:39:02 +0530

    buuble

---
+++ Array/sorting/bubblesort.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void bubblesort(int arr[], int n) {
+    for (int i = 0; i < n-1; i++) {
+        for (int j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                // Swap arr[j] and arr[j+1]
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
+int main(){
+    int arr[];
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    printf("Enter the elements: ");
+}
